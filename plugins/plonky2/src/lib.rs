7ce3d34a2f2b7cd50bad9b41347c01ff7f7e2683
Brandon H. Gomes <bhgomes@pm.me>
2022-11-01 13:18:24 -0700

    chore: set up Plonky2 dependency
    Signed-off-by: Brandon H. Gomes <bhgomes@pm.me>

---
+++ plugins/plonky2/src/lib.rs
@@ -0,0 +1,9 @@
+//! Plonky2 OpenZL Plugin
+
+#![cfg_attr(not(any(feature = "std", test)), no_std)]
+#![cfg_attr(doc_cfg, feature(doc_cfg))]
+#![forbid(rustdoc::broken_intra_doc_links)]
+#![forbid(missing_docs)]
+
+#[doc(inline)]
+pub use plonky2;
